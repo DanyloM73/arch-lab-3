5baa52ed1e1abdd3bc7e8665fea5564819b872c3
makskhv21 <khvits21@gmail.com>
2024-04-22 13:46:21 +0300

    Optimized command parsing in Parse method

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -1,6 +1,11 @@
 package lang
 
 import (
+	""bufio"
+	"fmt"
+	"image/color""
+	"strconv"
+	"strings"
 	"io"
 
 	"github.com/DanyloM73/arch-lab-3/painter"
@@ -11,11 +16,27 @@ type Parser struct {
 }
 
 func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
-	var res []painter.Operation
+	scanner := bufio.NewScanner(in)
+	scanner.Split(bufio.ScanLines)
 
-	// TODO: Реалізувати парсинг команд.
-	res = append(res, painter.OperationFunc(painter.WhiteFill))
-	res = append(res, painter.UpdateOp)
+	var result []painter.Operation
+	for scanner.Scan() { 
+		commandLine := scanner.Text()
+		oprtn := parse(commandLine) 
+		if oprtn == nil {
+			return nil, fmt.Errorf("Failed to parse this command: %s", commandLine)
+		}
 
-	return res, nil
+		// Replace any previous BgRectangle operation with the new one
+		if bgRect, ok := oprtn.(*painter.BgRectangle); ok {
+			for i, oldOp := range result {
+				if _, ok := oldOp.(*painter.BgRectangle); ok {
+					result[i] = bgRect
+					break
+				}
+			}
+		}
+		result = append(result, oprtn)
+	}
+	return result, nil
 }
