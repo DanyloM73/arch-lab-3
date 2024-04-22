bc004d51f7665a4e11fa57543f7e2a27bf3dbeea
makskhv21 <khvits21@gmail.com>
2024-04-22 13:50:42 +0300

    Improved command parsing in parse function

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -40,3 +40,43 @@ func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
 	}
 	return result, nil
 }
+
+func parse(commandLine string) painter.Operation {
+	parts := strings.Split(commandLine, " ")
+	instruction := parts[0]
+	var args []string
+	if len(parts) > 1 {
+		args = parts[1:]
+	}
+	var iArgs []int
+	for _, arg := range args {
+		i, err := strconv.Atoi(arg)
+		if err == nil {
+			iArgs = append(iArgs, i)
+		}
+	}
+
+	var figureOps []painter.Figure
+
+	switch instruction {
+	case "white":
+		return painter.OperationFunc(painter.WhiteFill)
+	case "green":
+		return painter.OperationFunc(painter.GreenFill)
+	case "bgrect":
+		return &painter.BgRectangle{x1: iArgs[0], y1: iArgs[1], x2: iArgs[2], y2: iArgs[3]}
+	case "figure":
+		clr := color.RGBA{R: 255, G: 255, B: 0, A: 1}
+		figure := painter.Figure{x: iArgs[0], y: iArgs[1], c: clr}
+		figureOps = append(figureOps, figure)
+		return &figure
+	case "move":
+		return &painter.Move{x: iArgs[0], y: iArgs[1], Figures: figureOps}
+	case "reset":
+		figureOps = figureOps[0:0]
+		return painter.OperationFunc(painter.ResetScreen)
+	case "update":
+		return painter.UpdateOp
+	}
+	return nil
+}
